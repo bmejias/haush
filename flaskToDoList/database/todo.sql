932bbf37e34f5a38f804e8a3e8d1a15500b287b2
Boriss Mejias <boriss.mejias@enterprisedb.com>
2024-01-10 22:53:17 +0100

    Insert test data and task_id should be serial

--- flaskToDoList/database/todo.sql
+++ flaskToDoList/database/todo.sql
@@ -1,11 +1,29 @@
 CREATE SCHEMA IF NOT EXISTS todo;
 
 CREATE TABLE IF NOT EXISTS todo.tasks (
-    task_id             integer PRIMARY KEY
-  , task_name           text NOT NULL
-  , task_description    text
-  , due_date            timestamptz
-  , priority            integer
-  , status              text
-  , created_at          timestamptz default current_timestamp
+      task_id             serial PRIMARY KEY
+    , task_name           text NOT NULL
+    , task_description    text
+    , due_date            timestamptz
+    , priority            integer
+    , status              text
+    , created_at          timestamptz default current_timestamp
+);
+
+-- Inserting the first task
+INSERT INTO tasks (task_name, task_description, due_date, priority, status)
+VALUES ('Complete Project Report'
+    , 'Finish writing and editing the project report'
+    , '2023-01-31 18:00:00'
+    , 2
+    , 'To Do'
+);
+
+-- Inserting the second task
+INSERT INTO tasks (task_name, task_description, due_date, priority, status)
+VALUES ('Prepare Presentation'
+    , 'Create slides for the project presentation'
+    , '2023-02-15 14:30:00'
+    , 1
+    , 'In Progress'
 );
