1c9db1b4217da1656280ff9f02ddf1cc461bbf59
Boriss Mejias <boriss.mejias@enterprisedb.com>
2024-01-14 02:38:34 +0100

    Get all tasks on overview

--- flaskToDoList/main.py
+++ flaskToDoList/main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from model.task import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
     return '<p>Hello Patagonia!</p>'
+
+
+@app.route('/overview')
+def overview():
+    task_model = Task()
+    all_tasks = task_model.get_tasks()
+    return render_template("overview.html", all_tasks=all_tasks)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- flaskToDoList/model/task.py
+++ flaskToDoList/model/task.py
@@ -5,3 +5,8 @@ class Task():
     def __init__(self):
         database = Database()
         self.cursor, self.conn = database.connect_db()
+
+
+    def get_tasks(self):
+        self.cursor.execute("SELECT * FROM todo.tasks")
+        return self.cursor.fetchall()
